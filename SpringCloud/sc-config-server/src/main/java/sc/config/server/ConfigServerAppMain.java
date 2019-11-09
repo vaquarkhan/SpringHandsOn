e2795b8465e397e79100caf12fefa07037f05d98
javakishore-veleti <javakishore@gmail.com>
2019-11-08 22:15:57 -0500

    Initial version of Spring Cloud hands-on implementation

---
+++ SpringCloud/sc-config-server/src/main/java/sc/config/server/ConfigServerAppMain.java
@@ -0,0 +1,14 @@
+package sc.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerAppMain {
+
+    public static void main(String args[]) {
+        SpringApplication.run(ConfigServerAppMain.class, args);
+    }
+}
---
+++ SpringCloud/src/test/java/jk-spring-cloud/AppTest.java
@@ -0,0 +1,20 @@
+package jk
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+-spring-cloud;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
